00f4e6a0261556f843d6ec998bb2a7cece76dd10
Duncan Stevenson-Price <duncan@psmail.co>
2025-02-18 08:49:29 +0000

    Initial commit

---
+++ dot_config/zsh/resources/aliases.zsh
@@ -0,0 +1,8 @@
+# Aliases for eza
+# eza https://eza.rocks/
+alias ls="eza -a --icons"
+alias ll="eza -al --no-user --time-style long-iso --icons --git"
+alias lt="eza -aTL 3 --icons"
+
+# bar is a better cat with syntax highlighting
+alias cat="bat"
