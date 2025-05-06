19e6893e6eb491d519aa0bc2ed1fa28df402244a
Duncan Stevenson-Price <duncan@psmail.co>
2025-05-06 13:00:01 +0100

    Stop direnv moaning

--- dot_config/zsh/resources/test-track.zsh
+++ dot_config/zsh/resources/test-track.zsh
@@ -1,6 +1,9 @@
 # Enable DirEnv, used by the Core toolchain
 eval "$(direnv hook zsh)"
 
+# Stop Direnv moaning about taking ages even though it ALWAYS takes ages when Nix is involved
+export DIRENV_WARN_TIMEOUT=0
+
 # GitLab Tokens
 export GITLAB_TOKEN="op://Employee/GitLab/Personal Access Tokens/Read API"
 alias yarn="op run --account agilebits --no-masking -- yarn"
